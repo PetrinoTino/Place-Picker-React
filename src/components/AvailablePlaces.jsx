6f9ca826890732174fc36823d6f6e1d71c7f555e
Petrino Qasolli <petrinotino@gmail.com>
2025-04-04 12:06:57 +0200

    formt document

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,49 +1,46 @@
-import { useEffect, useState } from 'react';
-import Places from './Places.jsx';
-import Error from './Error.jsx';
-import { sortPlacesByDistance } from '../loc.js';
-import fetchAvaiablePlaces from '../http.js'
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import fetchAvaiablePlaces from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching]=useState(false);
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
-const[error,setError]=useState();
+  const [error, setError] = useState();
   useEffect(() => {
-
-   
     // menyra e 2 per te perdorur async/await ne nje use effect duke krijuar variabla const gjithmone
-async function fetchPlaces(){
- setIsFetching(true);
+    async function fetchPlaces() {
+      setIsFetching(true);
 
- try{
+      try {
+        const places = await fetchAvaiablePlaces();
 
-const places=await fetchAvaiablePlaces();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
 
-navigator.geolocation.getCurrentPosition((position) => {
-  
-  const sortedPlaces=sortPlacesByDistance(
-    places,
-    position.coords.latitude,
-    position.coords.longitude);
+          setAvailablePlaces(sortedPlaces);
 
-  setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
 
-  setIsFetching(false);
-
-});
-
-
-//ok 200,300 ,not ok 400,500
- }catch(error){
-setError({message: error.message || 'Could not fetch places,please try again latter!'})
-
- }
-setIsFetching(false);
+        //ok 200,300 ,not ok 400,500
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places,please try again latter!",
+        });
+      }
+      setIsFetching(false);
     }
 
-fetchPlaces();
+    fetchPlaces();
 
-// Menyra e pare per te perdorur fetch 
+    // Menyra e pare per te perdorur fetch
     // fetch('http://localhost:3000/places')
     //   .then((response) => response.json())
     //   .then((resData) => {
@@ -51,15 +48,15 @@ fetchPlaces();
     //   });
   }, []);
 
-  if(error){
-    return <Error title="An error occurred!" message={error.message} />
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
   }
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       isLoading={isFetching}
-loadingText="Fetching place data..."
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
