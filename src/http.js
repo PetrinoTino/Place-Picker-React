9f5cd0170dde7c8defd86f60fb828255d4a84a50
Petrino Qasolli <petrinotino@gmail.com>
2025-04-16 16:00:52 +0200

    e

--- src/http.js
+++ src/http.js
@@ -10,21 +10,3 @@ export async function fetchAvaiablePlaces() {
 }
 export default fetchAvaiablePlaces;
 
-// export async function updateUserPlaces(places){
-//  const response=await  fetch('http://localhost:3000/user-places',{
-
-// method: 'PUT',
-// body: JSON.stringify(places),
-// headers:{
-//   'Content-Type': ' application/json'
-// }
-// });
-
-// const resData = await response.json();
-// if (!response.ok){
-//   throw new Error('Failed to update user data.')
-
-// }
-// return  resData.message
-// }
-// export default updateUserPlaces;
